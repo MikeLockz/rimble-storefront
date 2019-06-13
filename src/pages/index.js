af537661ae54682bfde7bb43324f11169cf00e16
ryancreatescopy <40891631+ryancreatescopy@users.noreply.github.com>
2019-06-13 16:19:46 +0100

    Added demo link and tweaked some copy (#22)

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import RimbleGatsbyLink from "../components/landing/RimbleGatsbyLink";
 import ThemeProvider from "../components/landing/ThemeProvider";
 import Typography from "../components/landing/typography";
 import Container from "../components/landing/Container";
+import CodeBlock from "../components/landing/CodeBlock";
 import Heading from "../components/landing/Heading";
 import Text from "../components/landing/Text";
 import Hr from "../components/landing/Hr";
@@ -39,6 +40,22 @@ const Masthead = styled(Flex)`
   ${backgroundImage};
 `;
 
+const exampleCode = `
+import React, { Component } from 'react'
+
+import { Button } from 'rimble-ui'
+
+class Example extends Component {
+  render () {
+    return (
+      <Button size={'medium'}>
+        Click me!
+      </Button>
+    )
+  }
+}
+`;
+
 const IndexPage = () => (
   <ThemeProvider theme={defaultTheme}>
     <React.Fragment>
@@ -67,74 +84,63 @@ const IndexPage = () => (
 
               <Flex mt={6} width={1} flexDirection={["column", "row"]}>
                 <Box flex={"1 1 auto"} width={[1, 1 / 3]} mr={5}>
-                  <Flex
-                    flexDirection={"column"}
-                    justifyContent={"space-between"}
-                  >
-                    <Flex my={3}>
-                      <ReactSVG src="/static/svg/fig-1.svg" />
-                    </Flex>
-                    <Heading.h2 fontSize={[3, 4]} fontWeight={2}>
-                      UI
-                    </Heading.h2>
-                    <Text.p mt={"1"}>
-                      Clean, customisable building blocks for putting your
-                      interface together, including UI for Ethereum concepts.{" "}
-                    </Text.p>
-                    <RimbleGatsbyLink to="/components/rimble-ui">
-                      <Button width={["100%", "auto"]} my={"3"}>
-                        See installation instructions
-                      </Button>
-                    </RimbleGatsbyLink>
+                  <Flex flexDirection={"column"} justifyContent={"space-between"}>
+                  <Flex my={3}>
+                    <ReactSVG src="/static/svg/fig-1.svg" />
                   </Flex>
+                  <Heading.h2 fontSize={[3, 4]} fontWeight={2}>
+                    UI
+                  </Heading.h2>
+                  <Text.p mt={"1"}>
+                    Clean, customisable building blocks for putting your interface together, including UI for Ethereum concepts.
+                    {" "}
+                  </Text.p>
+                  <RimbleGatsbyLink to="/components/rimble-ui">
+                    <Button
+                      width={["100%", "auto"]} my={"3"}>
+                      Read documentation
+                    </Button>
+                  </RimbleGatsbyLink>
+                </Flex>
                 </Box>
                 <Box flex={"1 1 auto"} width={[1, 1 / 3]} mr={5}>
-                  <Flex
-                    flexDirection={"column"}
-                    justifyContent={"space-between"}
-                  >
-                    <Flex my={3}>
-                      <ReactSVG src="/static/svg/fig-2.svg" />
-                    </Flex>
-                    <Heading.h2 fontSize={[3, 4]} fontWeight={2}>
-                      Web3 components
-                    </Heading.h2>
-                    <Text.p mt={"1"}>
-                      Drop-in components that solve an Ethereum UX problem out
-                      of the box. Configure some inputs and you're ready.{" "}
-                    </Text.p>
-                    <RimbleGatsbyLink to="/components/web3-components">
-                      <Button width={["100%", "auto"]} my={"3"}>
-                        Read documentation
-                      </Button>
-                    </RimbleGatsbyLink>
+                  <Flex flexDirection={"column"} justifyContent={"space-between"}>
+                  <Flex my={3}>
+                    <ReactSVG src="/static/svg/fig-2.svg" />
                   </Flex>
+                  <Heading.h2 fontSize={[3, 4]} fontWeight={2}>
+                    Web3 components
+                  </Heading.h2>
+                  <Text.p mt={"1"}>
+                    Drop-in components that solve an Ethereum UX problem out of the box. Configure some inputs and you're ready.
+                    {" "}
+                  </Text.p>
+                  <RimbleGatsbyLink to="/components/web3-components">
+                    <Button
+                      width={["100%", "auto"]} my={"3"}>
+                      Read documentation
+                    </Button>
+                  </RimbleGatsbyLink>
+                </Flex>
                 </Box>
                 <Box flex={"1 1 auto"} width={[1, 1 / 3]}>
-                  <Flex
-                    flexDirection={"column"}
-                    justifyContent={"space-between"}
-                  >
-                    <Flex my={3}>
-                      <ReactSVG src="/static/svg/fig-3.svg" />
-                    </Flex>
-                    <Heading.h2 fontSize={[3, 4]} fontWeight={2}>
-                      Design patterns
-                    </Heading.h2>
-                    <Text.p mt={"1"}>
-                      In-depth pieces on designing patterns and workflows that
-                      help your users navigate Ethereum.
-                    </Text.p>
-                    <RimbleGatsbyLink to="/guides">
-                      <Button
-                        target="_blank"
-                        rel="noopener"
-                        width={["100%", "auto"]}
-                        my={"3"}
-                      >
-                        Read guides
-                      </Button>
-                    </RimbleGatsbyLink>
+                  <Flex flexDirection={"column"} justifyContent={"space-between"}>
+                  <Flex my={3}>
+                    <ReactSVG src="/static/svg/fig-3.svg" />
+                  </Flex>
+                  <Heading.h2 fontSize={[3, 4]} fontWeight={2}>
+                    Design patterns
+                  </Heading.h2>
+                  <Text.p mt={"1"}>
+                    In-depth pieces on designing patterns and workflows that help your users navigate the UX challenges of Ethereum.
+                  </Text.p>
+                  <RimbleGatsbyLink to="/guides">
+                  <Button
+                    target='_blank' rel='noopener'
+                    width={["100%", "auto"]} my={"3"}>
+                    Read guides
+                  </Button>
+                  </RimbleGatsbyLink>
                   </Flex>
                 </Box>
               </Flex>
@@ -146,19 +152,20 @@ const IndexPage = () => (
           <Container>
             <Box mt={6} mb={5}>
               <Heading.h1 fontSize={[4, 5]}>
-                Made by ConsenSys Design
+                See Rimble in action
               </Heading.h1>
               <Hr />
               <Text.p>
-                Rimble is created by{" "}
-                <RimbleLink
-                  href="//consensys.design/"
+                Take a look at our demo dApp. We prototype nearly everything we ship there first. Just remember it's our building site, so things might not always look finished. Hard hats are advised 👷‍♀️
+              </Text.p>
+                <Button
+                  as="a"
+                  href="https://rimble-app-demo.netlify.com/"
                   target="_blank"
                   rel="noopener"
                 >
-                  ConsenSys Design.
-                </RimbleLink>{" "}
-              </Text.p>
+                  Try the demo
+                </Button>
             </Box>
           </Container>
         </section>
