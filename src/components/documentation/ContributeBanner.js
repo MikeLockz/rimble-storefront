d661421157cb731d3b1c1d1e40089083146a6328
Ryan Cordell <ryan.cordell@consensys.net>
2019-10-29 14:21:39 +0000

    Improved contribution banner copy/design

--- src/components/documentation/ContributeBanner.js
+++ src/components/documentation/ContributeBanner.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Box, Card, Text, Heading } from "rimble-ui";
+import { Button, Box, Card, Text, Icon, Heading, Flex, Image } from "rimble-ui";
 
 const getContributeUrl = componentType => {
   switch (componentType) {
@@ -19,11 +19,12 @@ const ContributeBanner = ({ componentType }) => (
         <Text textAlign={"center"} mb={4}>
           {"(ﾉ◕ヮ◕)ﾉ*:・ﾟ✧"}
         </Text>
-        <Card color="white" bg="primary" border="none">
-          <Heading.h2 color="white">Are we missing anything?</Heading.h2>
+        <Card color="white" bg="primary-light" borderRadius="2" border="none">
+          <Flex>
+          <Box width={[3/4]}>
+          <Heading.h2 mb={3} color="white">Contribute to Rimble</Heading.h2>
           <Text color="white" mb={4}>
-            If you have any extra insight you'd like to add, please raise an
-            issue in Github.
+            We welcome pull requests and issues that help improve Rimble!
           </Text>
           <Button
             mainColor={"white"}
@@ -33,8 +34,20 @@ const ContributeBanner = ({ componentType }) => (
             title="Contribute to Rimble"
             target="_blank"
           >
-            Contribute
+            Contribute <Icon ml={2} name="Launch" size="16px" />
           </Button>
+          </Box>
+          <Box width={[1/4]}
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between">
+            <Image ml={2}
+              src="/images/GitHub-Mark-Light.png"
+              alt="Github logo"
+              height="auto"
+            />
+          </Box>
+          </Flex>
         </Card>
       </Box>
     ) : (
