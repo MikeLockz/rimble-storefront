d19de3de3976ce8ec2764697e00aa17de0646c76
Mike Lock <michael.lockwitz@consensys.net>
2019-11-05 14:35:58 -0700

    Wrapped Card contents in Flex to remove white gap between Image and Box border.

--- src/components/documentation/ComponentCard.js
+++ src/components/documentation/ComponentCard.js
@@ -4,22 +4,30 @@ import RimbleGatsbyLink from "./../landing/RimbleGatsbyLink";
 
 const ComponentCard = ({ componentName, image, imageAlt, description }) => {
   return (
-    <Box width={[1, 1 / 2]} mt={3} p={2} >
+    <Box width={[1, 1 / 2]} mt={3} p={2}>
       <RimbleGatsbyLink
         to={`/components/rimble-ui/${componentName}`}
         title={`Code and usage examples for the ${componentName} component`}
       >
-      <Card p={0} mx={"auto"} height={"100%"}>
-        <Image
-          alt={`Example of a Rimble ${componentName} component`}
-          width={1}
-          src={`/images/${componentName}.png`}
-        />
-      <Box px={4} py={3} borderTop={1} borderWidth={3} borderColor="primary-light">
-        <Heading.h4 my={3}>{componentName}</Heading.h4>
-        <Text mb={3}>{description}</Text>
-      </Box>
-      </Card>
+        <Card p={0} mx={"auto"} height={"100%"}>
+          <Flex flexDirection={"column"}>
+            <Image
+              alt={`Example of a Rimble ${componentName} component`}
+              width={1}
+              src={`/images/${componentName}.png`}
+            />
+            <Box
+              px={4}
+              py={3}
+              borderTop={1}
+              borderWidth={3}
+              borderColor="primary-light"
+            >
+              <Heading.h4 my={3}>{componentName}</Heading.h4>
+              <Text mb={3}>{description}</Text>
+            </Box>
+          </Flex>
+        </Card>
       </RimbleGatsbyLink>
     </Box>
   );
