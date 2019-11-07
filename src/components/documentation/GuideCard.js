17f4d01564cc67068434681c7d26aaf2e12887b6
Ryan Cordell <ryan.cordell@consensys.net>
2019-11-07 09:04:38 +0000

    Made stacked buttons full-width in GuideCard

--- src/components/documentation/GuideCard.js
+++ src/components/documentation/GuideCard.js
@@ -13,36 +13,45 @@ return (
         src={GuideImage}
       />
       <Box borderTop={1} borderWidth={3} borderColor="primary-light" px={4} py={3}>
-      <Heading.h2 py={3}>{GuideTitle}</Heading.h2>
-      <Heading.h5 py={2} color="#666">{GuideSubTitle}</Heading.h5>
-      <Text my={3}>
-        {GuideDescription}
-      </Text>
-        {children}
+        <Heading.h2 py={3}>{GuideTitle}</Heading.h2>
+        <Heading.h5 py={2} color="#666">{GuideSubTitle}</Heading.h5>
+        <Text my={3}>
+          {GuideDescription}
+        </Text>
+          {children}
       </Box>
     </Flex>
       <Box borderTop={1} borderWidth={1} borderColor="light-gray">
-        <Flex my={3} mx={3} flexDirection={["column", "row"]} alignItems={["center", "stretch"]} justifyContent={["center", "flex-end"]} flexWrap={'wrap'}>
-          <Flex alignItems="center" mb={[3, 2]} mt={[2, 0]} mx={3}>
-            <Link
-              fontSize={2}
-              href={DemoLink}
-              title={DemoTitle}
-              target="_blank">
-              <Flex alignItems="center">
-              {DemoCTA}
-              <Icon ml={2} color="primary" size="16px" name="Launch" />
-              </Flex>
-            </Link>
-          </Flex>
-          <RimbleGatsbyLink
-            to={GuideLink}
-            title={GuideLinkTitle}
-          >
-            <Button mx={[0, 3]} mb={2}>
-            {GuideCTA}
-            </Button>
-          </RimbleGatsbyLink>
+        <Flex my={3} mx={3} flexDirection={["column", "row"]} alignItems="center" justifyContent={["center", "flex-end"]} flexWrap={'wrap'}>
+          <Box width={[1, "auto"]}>
+            <Flex justifyContent="center" alignItems="center" mb={[3, 2]} mt={[2, 0]} mx={3}>
+              <Link
+                fontSize={2}
+                href={DemoLink}
+                title={DemoTitle}
+                target="_blank"
+                mr={[0, 3]}
+                >
+                <Flex alignItems="center">
+                {DemoCTA}
+                <Icon ml={2} color="primary" size="16px" name="Launch" />
+                </Flex>
+              </Link>
+            </Flex>
+          </Box>
+          <Box width={[1, "auto"]}>
+            <RimbleGatsbyLink
+              to={GuideLink}
+              title={GuideLinkTitle}
+            >
+              <Button
+                mr={[0, 4]}
+                mb={2}
+                width="100%">
+              {GuideCTA}
+              </Button>
+            </RimbleGatsbyLink>
+          </Box>
         </Flex>
       </Box>
     </Card>
