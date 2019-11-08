6e5ee3ab34dc2fa31718032c11eab3e0abedd9a0
Ryan Cordell <ryan.cordell@consensys.net>
2019-11-08 14:11:41 +0000

    Spiffed up 404 page with more helpful next steps
    For some reason `components` page is not building on this branch. Need to investigate.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,51 @@
 import React from "react";
 import SEO from "../components/global/SEO";
-import { Box, Heading, Text } from "rimble-ui";
+import { Box, Flex, Card, Heading, Text, Image, Link } from "rimble-ui";
+import SearchInput from "../components/documentation/SearchInput";
 
 const NotFoundPage = () => (
-  <Box p={4}>
+
+  <Box width="auto" p={4} justifyContent="center">
     <SEO title="404: Not found" />
-    <Heading.h1>NOT FOUND</Heading.h1>
-    <Text>You just hit a route that doesn&#39;t exist... the sadness.</Text>
+      <Flex justifyContent={["center", "flex-start"]}>
+      <Image src="/rimble-grey.svg"
+      alt="Rimble logo"
+      mb={5}/>
+      </Flex>
+    <Flex justifyContent="center">
+      <Heading.h1 color="primary">0x404...</Heading.h1>
+    </Flex>
+    <Flex justifyContent="center" my={3}>
+      <Heading.h1 textAlign="center">PAGE NOT FOUND</Heading.h1>
+    </Flex>
+    <Flex justifyContent="center" my={3}>
+      <Text textAlign="center">We must have hard-forked since this page existed.</Text>
+    </Flex>
+    <Flex justifyContent="center">
+    <Card border={0} borderTop={1} borderColor="primary-light" borderWidth={3} mt={3} width={[1, 1/2]}>
+      <Text caps fontWeight="bold" fontSize={1}>Search our docs</Text>
+      <SearchInput my={3}/>
+    <Box mt={3} pt={2} borderTop={1} borderColor="near-white">
+      <Text caps fontWeight="bold"  fontSize={1} mt={3}>Quick links</Text>
+      <Flex alignItems="center" my={3}>
+        <Link
+          href="/"
+          title="To Rimble homepage"> Home</Link>
+      </Flex>
+      <Flex my={3}>
+        <Link
+          href="/components"
+          title="To Rimble components"> Components</Link>
+      </Flex>
+      <Flex mt={3}>
+        <Link
+          href="/guides"
+          title="To Rimble Guides"> Guides</Link>
+      </Flex>
+    </Box>
+    </Card>
+    </Flex>
+
   </Box>
 );
 
