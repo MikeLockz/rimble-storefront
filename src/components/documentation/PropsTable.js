87fce8191b494c2ebf4c2984569ae2984a867ac0
Ryan Cordell <ryan.cordell@consensys.net>
2019-11-11 15:01:30 +0000

    Props table -> list

--- src/components/documentation/PropsTable.js
+++ src/components/documentation/PropsTable.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
-import { Table, Box, Text } from "rimble-ui";
+import { Table, Box, Text, Heading, Flex } from "rimble-ui";
 
 const StyledTable = styled(Table)`
   & {
@@ -20,67 +20,61 @@ const StyledTable = styled(Table)`
   }
 `;
 
-const Code = props => <Text as="pre" fontSize={1} {...props} />;
+const Code = props => <Text fontSize={2} {...props} />;
 
 const PrettyPrintJson = React.memo(({ data }) => {
   return (
-    <Box>
-      <Code>{data}</Code>
-    </Box>
+      <Box>
+      <Text fontWeight="bold">Default value:</Text>
+        <Code>{data}</Code>
+      </Box>
   );
 });
 
 const PropsTable = props => {
   let { propMetaData = [], ..._props } = props;
   return (
-    <Box my={4}>
+    <Box bg="#FCFCFC" borderRadius={4} my={4}>
       {propMetaData.length > 0 ? (
-        <StyledTable {..._props}>
-          <thead>
-            <tr>
-              <th>Prop Name</th>
-              <th>Type</th>
-              <th>Is Required</th>
-              <th>Default Value</th>
-              <th>Description</th>
-            </tr>
-          </thead>
-          <tbody>
+        <Box p={3} {..._props}>
+          <Box>
             {Object.keys(propMetaData).map(key => {
               const prop = propMetaData[key];
               return (
-                <tr key={key}>
-                  <td>{prop.name}</td>
-                  <td>{prop.parentType ? prop.parentType.name : ""}</td>
-                  {prop.required ? <td>required</td> : <td>optional</td>}
-                  {prop.defaultValue ? (
-                    <td>
-                      <PrettyPrintJson data={prop.defaultValue.value} />
-                    </td>
-                  ) : (
-                    <td>none</td>
+                <Box my={3} borderBottom={1} borderWidth={2} borderColor="light-gray" key={key}>
+                <Flex alignItems="center" alignContent="center" flexWrap={'wrap'} alignItems={"stretch"}>
+                  <Heading.h4 mr={2} fontFamily="monospace" color="primary-dark">{prop.name} </Heading.h4>
+                  <Text mr={2} fontFamily="monospace"  color="mid-gray">{prop.parentType ? prop.parentType.name : ""}</Text>
+                  <Text>{prop.required ? <Text fontFamily="monospace" color="mid-gray">(required)</Text> : <Text fontFamily="monospace" color="mid-gray">(optional)</Text>}</Text>
+                </Flex>
+                <Text italic mb={2}>
+                  {prop.docblock ? prop.docblock : ""}
+                  {prop.parentType && prop.parentType.name === "enum" && (
+                    <Box>
+                      <Text.span fontSize={2}>Allowed values: </Text.span>
+                      {Object.keys(prop.type.value).map(key => {
+                        const value = prop.type.value[key];
+                        return (
+                          <Text.span key={key} fontSize={1}>
+                            {value.value},{" "}
+                          </Text.span>
+                        );
+                      })}
+                    </Box>
                   )}
-                  <td>
-                    {prop.docblock ? prop.docblock : ""}
-                    {prop.parentType && prop.parentType.name === "enum" && (
-                      <Box>
-                        <Text.span fontSize={1}>Allowed values: </Text.span>
-                        {Object.keys(prop.type.value).map(key => {
-                          const value = prop.type.value[key];
-                          return (
-                            <Text.span key={key} fontSize={1}>
-                              {value.value},{" "}
-                            </Text.span>
-                          );
-                        })}
-                      </Box>
-                    )}
-                  </td>
-                </tr>
+                </Text>
+                  <Box mb={2} p={2}>{prop.defaultValue ? (
+                      <Text><PrettyPrintJson data={prop.defaultValue.value} /></Text>
+                  ) : (
+                    <Text></Text>
+                    //want to set it so that nothing shows if there is no default value
+                  )}
+                  </Box>
+                </Box>
               );
             })}
-          </tbody>
-        </StyledTable>
+          </Box>
+        </Box>
       ) : (
         <Text>No props defined for this component.</Text>
       )}
