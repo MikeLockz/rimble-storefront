4399747b45f525d25ff9c0f1ebc2cfe59bb1ba86
Mike Lock <michael.lockwitz@consensys.net>
2019-11-13 12:35:18 -0700

    Removed unused components. Added conditional border to bottom of props if not last in list. Adjusted spacing between props. Aligned default values to one row. Formatted JSON blocks for default values (Blockie).

--- src/components/documentation/PropsTable.js
+++ src/components/documentation/PropsTable.js
@@ -1,75 +1,103 @@
 import React from "react";
-import styled from "styled-components";
 import PropTypes from "prop-types";
-import { Table, Box, Text, Heading, Flex } from "rimble-ui";
-
-const StyledTable = styled(Table)`
-  & {
-    display: block;
-    width: 100%;
-    overflow: auto;
-    border-width: 0;
-  }
-
-  th,
-  td {
-    border: solid;
-    border-width: 1px;
-    border-color: inherit;
-    padding: 0 1.5rem;
-  }
-`;
+import { Box, Text, Heading, Flex } from "rimble-ui";
+import styled from "styled-components";
 
 const Code = props => <Text fontSize={2} {...props} />;
+const StyledPre = styled.pre`
+  margin: 0;
+`;
 
 const PrettyPrintJson = React.memo(({ data }) => {
   return (
-      <Box>
-      <Text fontWeight="bold">Default value:</Text>
-        <Code>{data}</Code>
-      </Box>
+    <Flex>
+      <Text fontWeight="bold" mr={2}>
+        Default value:
+      </Text>
+      <Code>
+        <StyledPre>{data}</StyledPre>
+      </Code>
+    </Flex>
   );
 });
 
 const PropsTable = props => {
   let { propMetaData = [], ..._props } = props;
   return (
-    <Box bg="#f5f5f5" border={1} borderWidth={2} borderColor="near-white" borderRadius={2} my={4}>
+    <Box
+      bg="#f5f5f5"
+      border={1}
+      borderWidth={2}
+      borderColor="near-white"
+      borderRadius={2}
+      my={4}
+    >
       {propMetaData.length > 0 ? (
         <Box p={3} {..._props}>
           <Box>
             {Object.keys(propMetaData).map(key => {
               const prop = propMetaData[key];
               return (
-                <Box my={3} borderBottom={1} borderWidth={2} borderColor="light-gray" key={key}>
-                <Flex alignItems="center" alignContent="center" flexWrap={'wrap'} alignItems={"stretch"}>
-                  <Heading.h4 mr={2} fontFamily="monospace" color="primary-dark">{prop.name} </Heading.h4>
-                  <Text mr={2} fontFamily="monospace"  color="mid-gray">{prop.parentType ? prop.parentType.name : ""}</Text>
-                  <Text>{prop.required ? <Text fontFamily="monospace" color="mid-gray">(required)</Text> : <Text fontFamily="monospace" color="mid-gray">(optional)</Text>}</Text>
-                </Flex>
-                <Text italic mb={2}>
-                  {prop.docblock ? prop.docblock : ""}
-                  {prop.parentType && prop.parentType.name === "enum" && (
-                    <Box>
-                      <Text.span fontSize={2}>Allowed values: </Text.span>
-                      {Object.keys(prop.type.value).map(key => {
-                        const value = prop.type.value[key];
-                        return (
-                          <Text.span key={key} fontSize={2}>
-                            {value.value}{" "}
-                          </Text.span>
-                        );
-                      })}.
-                    </Box>
-                  )}
-                </Text>
-                  <Box mb={2} p={2}>{prop.defaultValue ? (
-                      <Text><PrettyPrintJson data={prop.defaultValue.value} /></Text>
-                  ) : (
-                    <Text></Text>
-                    //want to set it so that nothing shows if there is no default value
-                  )}
+                <Box my={3} key={key}>
+                  <Flex
+                    alignItems="center"
+                    alignContent="center"
+                    flexWrap={"wrap"}
+                  >
+                    <Heading.h4
+                      mr={2}
+                      fontFamily="monospace"
+                      color="primary-dark"
+                    >
+                      {prop.name}{" "}
+                    </Heading.h4>
+                    <Text mr={2} fontFamily="monospace" color="mid-gray">
+                      {prop.parentType ? prop.parentType.name : ""}
+                    </Text>
+                    <Text>
+                      {prop.required ? (
+                        <Text fontFamily="monospace" color="mid-gray">
+                          (required)
+                        </Text>
+                      ) : (
+                        <Text fontFamily="monospace" color="mid-gray">
+                          (optional)
+                        </Text>
+                      )}
+                    </Text>
+                  </Flex>
+                  <Text italic mb={2}>
+                    {prop.docblock ? prop.docblock : ""}
+                    {prop.parentType && prop.parentType.name === "enum" && (
+                      <Box>
+                        <Text.span fontSize={2}>Allowed values: </Text.span>
+                        {Object.keys(prop.type.value).map(key => {
+                          const value = prop.type.value[key];
+                          return (
+                            <Text.span key={key} fontSize={2}>
+                              {value.value}{" "}
+                            </Text.span>
+                          );
+                        })}
+                        .
+                      </Box>
+                    )}
+                  </Text>
+                  <Box>
+                    {prop.defaultValue && (
+                      <PrettyPrintJson data={prop.defaultValue.value} />
+                    )}
                   </Box>
+
+                  {/* Add border bottom to all props except last */}
+                  {propMetaData.length !== parseInt(key) + 1 && (
+                    <Box
+                      p={2}
+                      borderBottom={1}
+                      borderWidth={2}
+                      borderColor="light-gray"
+                    />
+                  )}
                 </Box>
               );
             })}
